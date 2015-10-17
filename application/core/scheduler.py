436620611a661e76800638516fd531814822b9b8
Cameron Gray <cameron@camerongray.me>
2015-10-17 23:01:52 +0100

    Added "Maximum lateness" config value which will ensure plugins will not be executed too long after they were due

--- application/core/scheduler.py
+++ application/core/scheduler.py
@@ -6,6 +6,7 @@ from config import get_config, get_config_value
 
 config = get_config()
 scheduler_delay = get_config_value(config, "scheduler_delay")
+lateness_delta = timedelta(0, get_config_value(config, "max_lateness"))
 
 def scheduler_loop(callback):
     while True:
@@ -22,11 +23,16 @@ def scheduler_loop(callback):
 
         intervals = ScheduleInterval.query.filter(
             ScheduleInterval.execute_next < current_timestamp).filter(or_(
-            ScheduleInterval.last_executed < ScheduleInterval.execute_next,
-            ScheduleInterval.last_executed == None))
+                ScheduleInterval.last_executed < ScheduleInterval.execute_next,
+                ScheduleInterval.last_executed == None
+            ))
 
         for i in intervals:
-            callback(i.schedule)
+            # Only execute if it is not too late, however still walk the
+            # timestamp, otherwise late schedules will never execute again!
+            if i.execute_next >= current_timestamp - lateness_delta:
+                callback(i.schedule)
+
             (num_iterations, execute_next) = walk_execute_next(i.execute_next,
                 i.interval_seconds, current_timestamp)
             i.execute_next = execute_next
