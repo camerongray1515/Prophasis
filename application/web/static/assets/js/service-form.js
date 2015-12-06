5edf79e6ac4439a3b05276f373fc4904a7b4ebf2
Cameron Gray <cameron@camerongray.me>
2015-12-06 23:09:59 +0000

    Service form frontend complete

--- application/web/static/assets/js/service-form.js
+++ application/web/static/assets/js/service-form.js
@@ -8,28 +8,6 @@ serviceForm = {
     },
     addDependency: function(item) {
         item["icon"] = (item["type"] == "group") ? "object-group" : "desktop";
-        var html = ui.renderTemplate("template-dependency", item);
-
-        $("#dependency-container").append(html);
-    },
-    addItemClicked: function() {
-        serviceForm.nextAction = serviceForm.addItem;
-        serviceForm.actionGroup = $(this).closest(".service-dependency");
-        serviceForm.selectHost();
-    },
-    addItem: function(item) {
-        console.log("Item");
-        console.log(serviceForm.actionGroup);
-        console.log(item);
-    },
-    itemSelection: function() {
-        $("#host-selection-modal").modal("hide");
-
-        var item = {
-            "id": $(this).attr("data-item-id"),
-            "name": $(this).attr("data-item-name"),
-            "type": $(this).attr("data-item-type")
-        }
 
         // Check the item doesn't already exist before adding
         var exists = false;
@@ -44,16 +22,61 @@ serviceForm = {
         if (exists) {
             ui.showAlert("error", "That item already exists as a dependency");
         } else {
-            serviceForm.nextAction(item);
+            var html = ui.renderTemplate("template-dependency", item);
+            $("#dependency-container").append(html);
         }
+    },
+    addItemClicked: function() {
+        serviceForm.nextAction = serviceForm.addItem;
+        serviceForm.actionGroup = $(this).closest(".service-dependency");
+        serviceForm.selectHost();
+    },
+    addItem: function(item) {
+        item["icon"] = (item["type"] == "group") ? "object-group" : "desktop";
+
+        var exists = false;
+        $(serviceForm.actionGroup).find(".redundancy-group-item").each(function(index) {
+            var id = $(this).attr("data-item-id");
+            var type = $(this).attr("data-item-type");
+            if (item["id"] == id && item["type"] == type) {
+                exists = true;
+            }
+        });
+
+        if (exists) {
+            ui.showAlert("error", "That item already exists as a dependency");
+        } else {
+            var html = ui.renderTemplate("template-redundancy-group-item", item);
+            $(serviceForm.actionGroup).find(".dependency-item-container").append(html);
+        }
+    },
+    addRedundancyGroup: function() {
+        var html = ui.renderTemplate("template-redundancy-group", {});
+        $("#dependency-container").append(html);
+    },
+    itemSelection: function() {
+        $("#host-selection-modal").modal("hide");
+
+        var item = {
+            "id": $(this).attr("data-item-id"),
+            "name": $(this).attr("data-item-name"),
+            "type": $(this).attr("data-item-type")
+        }
+
+        serviceForm.nextAction(item);
     },
     removeDependency: function() {
         $(this).closest(".service-dependency").remove();
     },
+    removeRedundancyGroupItem: function() {
+        $(this).closest(".redundancy-group-item").remove();
+    },
     bindEventHandlers: function() {
         $("#add-dependency").click(serviceForm.addDependencyClicked);
+        $("#add-redundancy-group").click(serviceForm.addRedundancyGroup);
         $("button.select-item").click(serviceForm.itemSelection);
         $("#dependency-container").on("click", ".add-item", serviceForm.addItemClicked);
+        $("#dependency-container").on("click", ".btn-dependency-remove-item", serviceForm.removeRedundancyGroupItem);
         $("#dependency-container").on("click", ".dependency-remove", serviceForm.removeDependency);
     }
 }
