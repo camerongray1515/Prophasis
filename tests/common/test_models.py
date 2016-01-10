6d434a2c061d832ee8aee2e17952c3834c1771b6
Cameron Gray <cameron@camerongray.me>
2016-01-10 01:22:08 +0000

    Added unit test for getting the health of a host

--- tests/common/test_models.py
+++ tests/common/test_models.py
@@ -1,6 +1,6 @@
 import unittest
 from models import create_all, drop_all, session, Host, HostGroup,\
-    HostGroupAssignment
+    HostGroupAssignment, Plugin, PluginResult
 
 class TestGetHostMembersip(unittest.TestCase):
     def setUp(self):
@@ -76,17 +76,102 @@ class TestGetHostMembersip(unittest.TestCase):
 
         self.assertEqual(sorted(group_ids), [2,5,6,7,8])
 
-class TestGetServiceHealth(unittest.TestCase):
+class TestGetHostHeatlh(unittest.TestCase):
     def setUp(self):
-
         drop_all()
         create_all()
+        # Insert all plugins
+        for letter in ["A", "B", "C"]:
+            session.add(Plugin(id=letter, name=letter))
 
-        # Add Hosts
-        for i in range(1,10):
-            session.add(Host(id=i, name="Host {}".format(i)))
+        # Insert all hosts
+        for number in range(1,9):
+            session.add(Host(name=number))
 
+        # Mock the assigned_plugins method
+        Host.assigned_plugins = property(lambda s: Plugin.query.all())
 
+        # All plugins OK
+        session.add(PluginResult(host_id=1, plugin_id="A",
+            health_status="ok"))
+        session.add(PluginResult(host_id=1, plugin_id="B",
+            health_status="ok"))
+        session.add(PluginResult(host_id=1, plugin_id="C",
+            health_status="ok"))
+
+        # All plugins critical
+        session.add(PluginResult(host_id=2, plugin_id="A",
+            health_status="critical"))
+        session.add(PluginResult(host_id=2, plugin_id="B",
+            health_status="critical"))
+        session.add(PluginResult(host_id=2, plugin_id="C",
+            health_status="critical"))
+
+        # All plugins unknown
+        session.add(PluginResult(host_id=3, plugin_id="A",
+            health_status="unknown"))
+        session.add(PluginResult(host_id=3, plugin_id="B",
+            health_status="unknown"))
+        session.add(PluginResult(host_id=3, plugin_id="C",
+            health_status="unknown"))
+
+        # One plugin critical
+        session.add(PluginResult(host_id=4, plugin_id="A",
+            health_status="ok"))
+        session.add(PluginResult(host_id=4, plugin_id="B",
+            health_status="critical"))
+        session.add(PluginResult(host_id=4, plugin_id="C",
+            health_status="ok"))
+
+        # One plugin unknown
+        session.add(PluginResult(host_id=5, plugin_id="A",
+            health_status="ok"))
+        session.add(PluginResult(host_id=5, plugin_id="B",
+            health_status="ok"))
+        session.add(PluginResult(host_id=5, plugin_id="C",
+            health_status="unknown"))
+
+        # All plugins no_data (Host 6)
+
+        # One plugin no data
+        session.add(PluginResult(host_id=7, plugin_id="A",
+            health_status="ok"))
+        session.add(PluginResult(host_id=7, plugin_id="C",
+            health_status="minor"))
+
+        # Mixed results
+        session.add(PluginResult(host_id=8, plugin_id="A",
+            health_status="ok"))
+        session.add(PluginResult(host_id=8, plugin_id="B",
+            health_status="major"))
+        session.add(PluginResult(host_id=8, plugin_id="C",
+            health_status="minor"))
+
+        session.commit()
+
+    def test_all_ok(self):
+        self.assertEqual(Host.query.get(1).health, "ok")
+
+    def test_all_critical(self):
+        self.assertEqual(Host.query.get(2).health, "critical")
+
+    def test_all_unknown(self):
+        self.assertEqual(Host.query.get(3).health, "unknown")
+
+    def test_one_critical(self):
+        self.assertEqual(Host.query.get(4).health, "critical")
+
+    def test_one_unknown(self):
+        self.assertEqual(Host.query.get(5).health, "unknown")
+
+    def test_all_no_data(self):
+        self.assertEqual(Host.query.get(6).health, "no_data")
+
+    def test_one_no_data(self):
+        self.assertEqual(Host.query.get(7).health, "minor")
+
+    def test_mixed_results(self):
+        self.assertEqual(Host.query.get(8).health, "major")
 
 if __name__ == "__main__":
     unittest.main()
